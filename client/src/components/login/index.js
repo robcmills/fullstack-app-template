354404b2f79513b7ba6e931d6840474e7ad7932a
robcmills <rob@buildingconnected.com>
2018-07-30 21:40:37 -0700

    Implement beautiful login card with material-ui

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -1,5 +1,81 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import { withStyles } from '@material-ui/core/styles'
 
-const Login = () => <h1>Login</h1>
+class Login extends Component {
+	state = {
+		username: '',
+		password: '',
+	}
 
-export default Login
+	handleChange = name => event => {
+		console.log('handleChange', event.target.value)
+		this.setState({
+			[name]: event.target.value,
+		})
+	}
+
+	render() {
+		const { classes } = this.props;
+		return (
+			<div className={classes.login}>
+				<Card className={classes.card}>
+					<CardContent>
+						<form className={classes.container} noValidate autoComplete="off">
+							<TextField
+								id="username"
+								value={this.state.username}
+								onChange={this.handleChange('username')}
+								margin="normal"
+								placeholder="username"
+								fullWidth
+							/>
+							<TextField
+								id="password"
+								value={this.state.password}
+								onChange={this.handleChange('password')}
+								margin="normal"
+								placeholder="password"
+								fullWidth
+							/>
+						</form>
+					</CardContent>
+					<CardContent className={classes.actions}>
+						<Button
+							color="primary"
+							size="large"
+							variant="contained"
+						>
+							Login
+						</Button>
+					</CardContent>
+				</Card>
+			</div>
+		)
+	}
+}
+
+const styles = {
+	login: {
+		'align-items': 'center',
+		'justify-content': 'center',
+		bottom: 0,
+		display: 'flex',
+		left: 0,
+		position: 'absolute',
+		right: 0,
+		top: 0,
+	},
+	card: {
+		width: 420,
+	},
+	actions: {
+		'justify-content': 'center',
+		display: 'flex',
+	},
+}
+
+export default withStyles(styles)(Login)
