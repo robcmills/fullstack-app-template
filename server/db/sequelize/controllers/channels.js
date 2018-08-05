88af9291b0bd6661d1069d88eb102ce37245c9f0
robcmills <rob@buildingconnected.com>
2018-08-05 13:19:28 -0700

    Add controller method to create a channel

--- server/db/sequelize/controllers/channels.js
+++ server/db/sequelize/controllers/channels.js
@@ -8,3 +8,21 @@ module.exports.all = (req, res) => {
 		res.status(500).send('Error in Channel.findAll')
 	})
 }
+
+module.exports.create = (req, res, next) => {
+	Channel.findOne({ where: { name: req.body.name } }).then((existingChannel) => {
+		if (existingChannel) {
+			return res.status(409).send('A Channel with the provided name already exists')
+		}
+
+		const channel = Channel.build({
+			name: req.body.name
+		})
+
+		return channel.save().then(() => {
+			return res.status(200).json(channel)
+		})
+	}).catch(err =>
+		next(err)
+	)
+}
--- server/init/routes/channel-routes.js
+++ server/init/routes/channel-routes.js
@@ -2,4 +2,5 @@ const { controllers: { channels } } = require('../../db')
 
 module.exports = (app) => {
 	app.get('/api/channels', channels.all)
+	app.post('/api/channels', channels.create)
 }
