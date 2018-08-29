a59f2607bdfd10ba700e91fbd9de793d46bff241
robcmills <rob@buildingconnected.com>
2018-08-29 06:43:48 -0700

    Add root package.json and zip script to build source bundle for elastic beanstalk

---
+++ zip.sh
@@ -0,0 +1,14 @@
+zip -rv ./app.zip . \
+	-i \
+	client/build/asset-manifest.json \
+  client/build/manifest.json \
+  client/build/service-worker.js \
+  client/build/static/\* \
+  client/build/favicon.ico \
+  package.json \
+  server/\*.js \
+  server/package.json \
+  server/data/ \
+	-x \
+	\*node_modules/\* \
+	\*.DS_Store
