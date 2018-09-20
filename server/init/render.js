1e10f8d1717186976f7cb2441e714dce8c5c48ad
robcmills <rob@buildingconnected.com>
2018-09-20 07:48:43 -0700

    Fix bug in express static middleware by using static route

--- server/init/render.js
+++ server/init/render.js
@@ -12,8 +12,9 @@ const getIndexHtml = ({ initialState }) => indexHtml
 	.replace('__GOOGLE_SIGNIN_CLIENT_ID__', process.env.GOOGLE_SIGNIN_CLIENT_ID)
 
 module.exports = (app) => {
-	app.use(express.static(path.join(__dirname, '../../client/build'), { index: false }))
-	app.get('*', (req, res) => {
+	app.use('/static', express.static(
+		path.join(__dirname, '../../client/build/static'), { index: false }))
+	app.get('*', (req, res, next) => {
 		const initialState = { isLoggingIn: !req.user, user: req.user }
 		res.status(200).send(getIndexHtml({ initialState }))
 	})
