098fc4ebfb00d1d26867c3a1df5135f67fb76763
robcmills <rob@buildingconnected.com>
2018-09-25 22:02:20 -0700

    Make change to client proxy to enable goole-login redirects

--- client/src/components/login/google-login.js
+++ client/src/components/login/google-login.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 
-// TODO: Figure out why /api/google-login does not proxy to localhost:3001
-// from the Create React App dev server (localhost:3000)
-// For now this is a manual workaround
-const url = process.env.NODE_ENV === 'development'
-	? 'http://localhost:3001/api/google-login'
-	: '/api/google-login'
-
 const LOGO_SIZE = 22
 
 const GoogleLogin = () =>
 	<Button
-		href={url}
+		href="/api/google-login"
 		size="large"
 		variant="outlined"
 	>
